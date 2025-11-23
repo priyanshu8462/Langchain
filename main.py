44c8fc3bc0fcad74f42d5bb9ed1b37c6c892297c
priyanshu8462 <priyanshu20202@gmail.com>
2025-11-23 21:21:32 +0530

    add anthropic query mechanism

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import PromptTemplate
 import os
 
 load_dotenv()
 
+model = ChatAnthropic(model="claude-haiku-4-5-20251001")
+
 def main():
+    prompt = PromptTemplate.from_template("Explain about {topic}")
+    #above line can be also be written as
+    prompt = PromptTemplate(template="Explain about topic {topic}")
+
+    new_prompt = prompt.format(topic="Langchain")
     print("Hello from langchain-course! ")
+
+    chain = model.invoke(new_prompt)
+    print(chain)
 
 
 if __name__ == "__main__":
