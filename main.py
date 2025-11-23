7a14814f720255b1ef5aa72b832f4cbed554177d
priyanshu8462 <priyanshu20202@gmail.com>
2025-11-23 13:12:09 +0530

    Setting environment variables

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.environ.get("ANTHROPIC_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
